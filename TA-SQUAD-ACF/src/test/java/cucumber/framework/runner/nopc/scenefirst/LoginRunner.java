fbf9183bebfc25e229816e23217fbc04ddf16ad2
Alamanda Cathartica Florencea <112675246+mandacf@users.noreply.github.com>
2022-09-23 23:54:36 +0700

    revisi

--- TA-SQUAD-ACF/src/test/java/cucumber/framework/runner/nopc/scenefirst/LoginRunner.java
+++ TA-SQUAD-ACF/src/test/java/cucumber/framework/runner/nopc/scenefirst/LoginRunner.java
@@ -4,8 +4,8 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-features = {"src/main/resources/nopc/scenefirst/02dashboard.feature",
-		"src/main/resources/nopc/scenefirst/01login.feature"},
+features = {"src/test/resources/nopc/scenefirst/02dashboard.feature",
+		"src/test/resources/nopc/scenefirst/01login.feature"},
 glue = "cucumber.framework.runner.nopc.scenefirst",
 monochrome=true,
 plugin = {"pretty","html:target/nopc/htmlreport/LoginRunner.html",
